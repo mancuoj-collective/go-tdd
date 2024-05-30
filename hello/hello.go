8f123660d08849ad1da93c0577282bab501e21eb
mancuoj <mancuoj@gmail.com>
2024-05-30 23:40:41 +0800

    chore: update

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func main() {
-	fmt.Println("Hello, world")
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
