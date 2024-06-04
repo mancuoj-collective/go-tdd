558a450e98695ffd767163ae65408ba55e011723
mancuoj <mancuoj@gmail.com>
2024-06-04 21:53:22 +0800

    chore: update

--- 06_pointers/wallet.go
+++ 06_pointers/wallet.go
@@ -7,21 +7,23 @@ import (
 
 type Bitcoin int
 
-type Wallet struct {
-	balance Bitcoin
-}
-
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
-		return errors.New("insufficient funds")
+		return ErrInsufficientFunds
 	}
 	w.balance -= amount
 	return nil
