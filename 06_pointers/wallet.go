9d416af438b43a286581f18bac3f069baac223cc
mancuoj <mancuoj@gmail.com>
2024-06-04 21:50:38 +0800

    chore: update

--- 06_pointers/wallet.go
+++ 06_pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -14,6 +17,14 @@ func (b Bitcoin) String() string {
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("insufficient funds")
+	}
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
