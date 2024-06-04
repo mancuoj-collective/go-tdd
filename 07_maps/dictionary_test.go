df75c1b3a2b56a3b5372d793e6988189bd7a1d36
mancuoj <mancuoj@gmail.com>
2024-06-04 23:43:47 +0800

    chore: update

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -2,21 +2,23 @@ package maps
 
 import "testing"
 
-func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
 
-	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
-		want := "this is just a test"
+	dictionary.Add(word, definition)
+	assertDefinition(t, dictionary, word, definition)
+}
 
-		assertStrings(t, got, want)
-	})
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
 
-	t.Run("unknown word", func(t *testing.T) {
-		_, got := dictionary.Search("unknown")
-
-		assertError(t, got, ErrNotFound)
-	})
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
 }
 
 func assertStrings(t testing.TB, got, want string) {
