9927005c6f376b902a13aef7e6a28bd592507f0e
mancuoj <mancuoj@gmail.com>
2024-06-05 23:15:43 +0800

    chore: update

--- 09_mocking/countdown.go
+++ 09_mocking/countdown.go
@@ -5,12 +5,25 @@ import (
 	"io"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
