9926d2668abf5ab20e4319298a3e2f0a3ef70343
mancuoj <mancuoj@gmail.com>
2025-03-03 01:03:05 +0800

    crud maps

--- 00-basic/basic_test.go
+++ 00-basic/basic_test.go
@@ -202,3 +202,92 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(20))
 	})
 }
+
+// 07
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		AssertEqual(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		AssertError(t, err, ErrNotFound)
+	})
+}
+
+func AssertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	AssertEqual(t, got, definition)
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		AssertNoError(t, err)
+		AssertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+
+		AssertError(t, err, ErrWordExists)
+		AssertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+
+	AssertDefinition(t, dictionary, word, newDefinition)
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "test definition"}
+
+		err := dictionary.Delete(word)
+
+		AssertNoError(t, err)
+
+		_, err = dictionary.Search(word)
+
+		AssertError(t, err, ErrNotFound)
+	})
+
+	t.Run("non-existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Delete(word)
+
+		AssertError(t, err, ErrWordDoesNotExist)
+	})
+}
