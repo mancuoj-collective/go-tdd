3d80f06080919793d73336863ccac4247e3b7a38
mancuoj <mancuoj@gmail.com>
2025-07-21 01:26:09 +0800

    refactor

--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -6,16 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		actual := Hello("Chris")
 		expected := "Hello, Chris"
-		if actual != expected {
-			t.Errorf("actual %q, expected %q", actual, expected)
-		}
+		assertCorrectMessage(t, actual, expected)
 	})
 
 	t.Run("saying hello to world when an empty string is supplied", func(t *testing.T) {
 		actual := Hello("")
 		expected := "Hello, World"
-		if actual != expected {
-			t.Errorf("actual %q, expected %q", actual, expected)
-		}
+		assertCorrectMessage(t, actual, expected)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, actual, expected string) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("got %q want %q", actual, expected)
+	}
 }
